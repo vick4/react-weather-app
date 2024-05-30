e2bd8214b5053238fe5330ad633d5e9935624fa0
unknown <victorisaacblw@gmail.com>
2024-05-30 03:54:41 +0100

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": 'ce145479e6msh51687f8fded55d0p1677d9jsn189c9c9fff72',// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0d6f71817e506ac9768c545fe418e4f5"; // enter your key from openweather API
